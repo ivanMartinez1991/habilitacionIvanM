ea99c96d8a3e81aca7dcfd32e7c3934aa36dd6bb
IVAN MARTINEZ CUBIDES <ivan.martinez.mt@correo.usa.edu.co>
2021-12-28 07:09:44 -0500

    Desarrollode habilitacion

---
+++ src/main/java/com/usa/hab/habilitacion/repository/crudrepository/ProductCrudRepository.java
@@ -0,0 +1,12 @@
+package com.usa.hab.habilitacion.repository.crudrepository;
+
+import com.usa.hab.habilitacion.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ProductCrudRepository extends MongoRepository<Product, String> {
+    public List<Product> findByDescriptionContainingIgnoreCase(String description);
+    public List<Product> findByPriceLessThanEqual(Double price);
+
+}
